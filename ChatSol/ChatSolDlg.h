31ad0f7a7520ce708038e937744e60fa7eb49230
05053 <05053@DESKTOP-36VQN9T>
2024-10-31 18:15:05 +0900

    초기 커밋.

---
+++ ChatSol/ChatSolDlg.h
@@ -0,0 +1,38 @@
+﻿
+// ChatSolDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CChatSolDlg 대화 상자
+class CChatSolDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CChatSolDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CHATSOL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CEdit m_test_edit;
+	afx_msg void OnBnClickedTestBtn();
+	CStatic m_result_sex;
+};
